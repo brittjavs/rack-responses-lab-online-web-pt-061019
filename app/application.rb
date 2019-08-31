aa195b3332fe3da55ec47be08dba84b4632a6da7
Brittany Javalera <brittjavalera@gmail.com>
2019-08-31 22:02:09 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,9 +5,10 @@ def call
   
   resp.write "#{time}\n"
   
-  if time.hour > 12
-  resp.write "Good Afternoon!"
-else
+  if time.hour < 12
+    resp.write "Good Morning!"
+  else
+    resp.write "Good Afternoon!"
 end
 
 end
